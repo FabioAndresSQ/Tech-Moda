e631e07f79c45919e78ca8c88c9474e5fc1d6f47
Yireis <yireisrodriguez@gmail.com>
2022-11-02 11:12:55 -0500

    commit ensayo

--- app/src/main/java/com/techmoda/view/ui/activity/MainActivity.kt
+++ app/src/main/java/com/techmoda/view/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.techmoda
+package com.techmoda.view.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.techmoda.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
