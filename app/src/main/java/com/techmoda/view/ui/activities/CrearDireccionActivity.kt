a94749fb4d772a0c3222015fb6f09ed537e2523e
Fabio Sanchez <fabi0andr3s@gmail.com>
2022-11-10 10:39:55 -0500

    Upodated Sprint 4

---
+++ app/src/main/java/com/techmoda/view/ui/activities/CrearDireccionActivity.kt
@@ -0,0 +1,12 @@
+package com.techmoda.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.techmoda.R
+
+class CrearDireccionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_direccion)
+    }
+}
